6e2d00262a4d21db24152f7df2dc7c96dca81fca
Gavrilova <eug.gavrilova@gmail.com>
2022-09-16 20:09:14 -0700

    Fix task

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let user = 'John Doe';
+console.log(user);
 const student = 'Yauheniya';
 console.log(student);
 user = student;
@@ -12,30 +13,29 @@ console.log(test);
 test--;
 // in test now we have value: 20 (cause it mains to one type of data - number)
 console.log(test);
-test = true;
+const x = 20;
 // in test now we have: true
-console.log(test);
+console.log(test == x);
+// is that what you mean?
 let result = 1;
-const mv = [2, 3, 5, 8];
-for (let i = 0; i < mv.length; i++) {
-  result = result * mv[i];
+const a = [2, 3, 5, 8];
+for (let i = 0; i < a.length; i++) {
+  result = result * a[i];
 }
 console.log(result);
 let result1 = '';
-const y = [2, 5, 8, 15, 0, 6, 20, 3];
-let t = 0;
-for (; t < y.length; t++) {
-  if (y[t] < 10 && y[t] > 5) {
-    result1 = result1 + y[t];
+const b = [2, 5, 8, 15, 0, 6, 20, 3];
+for (let i = 0; i < b.length; i++) {
+  if (b[i] < 10 && b[i] > 5) {
+    result1 = result1 + b[i];
   }
 }
 console.log(result1);
-let wynik = '';
-const mw = [2, 5, 8, 15, 0, 6, 20, 3];
-let i = 0;
-for (; i < mw.length; i++) {
-  if (mw[i] % 2 == 0) {
-    wynik = wynik + mw[i];
+let result2 = '';
+const c = [2, 5, 8, 15, 0, 6, 20, 3];
+for (let i = 0; i < c.length; i++) {
+  if (c[i] % 2 == 0) {
+    result2 = result2 + c[i];
   }
 }
-console.log(wynik);
+console.log(result2);
